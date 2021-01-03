8aabc9836dcb3dc992c793c7b031cd53224a694d
Michael Ruettgers <michael@ruettgers.eu>
2021-01-03 22:05:38 +0100

    Version bump to 2.1.6

--- src/config.h
+++ src/config.h
@@ -4,7 +4,7 @@
 #include "Arduino.h"
 #include "Sensor.h"
 
-const char *VERSION = "2.1.5";
+const char *VERSION = "2.1.6";
 
 // Modifying the config version will probably cause a loss of the existig configuration.
 // Be careful!
